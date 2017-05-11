ff92c04f9e684fdfcd001dc55349c3b405ff5410
Keith Downing <Keith Downing@DESKTOP-N09VR5S>
2017-05-11 19:01:29 -0400

    5-11-17 Creating fishingChampion. 5-11-17 Player class added. 5-11-17 Scene class added. 5-11-17 TackleBox class added. 5-11-17 Map class added.

---
+++ fishingChampion/src/byui/cit260/fishingChampion/model/Map.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.fishingChampion.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Keith Downing
+ */
+public class Map implements Serializable{
+    //class instance variables.
+    private double rowCount;
+    private double columnCount;
+    private double currentRow;
+    private double currentColumn;
+    private double currentScene;
+    
+    
+}
