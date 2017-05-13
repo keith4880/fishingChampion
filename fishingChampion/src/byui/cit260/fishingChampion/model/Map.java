da0bc0787efcefbf556078302b83c3b3b95513a0
Keith Downing <Keith Downing@DESKTOP-N09VR5S>
2017-05-13 13:43:09 -0400

    5-11-17 Creating fishingChampion. 5-11-17 Player class added. 5-11-17 Scene class added. 5-11-17 TackleBox class added. 5-11-17 Map class added. 5-13-17 Map class modified and finished.

--- fishingChampion/src/byui/cit260/fishingChampion/model/Map.java
+++ fishingChampion/src/byui/cit260/fishingChampion/model/Map.java
@@ -18,6 +18,96 @@ public class Map implements Serializable{
     private double currentRow;
     private double currentColumn;
     private double currentScene;
+
+    public double getRowCount() {
+        return rowCount;
+    }
+
+    public void setRowCount(double rowCount) {
+        this.rowCount = rowCount;
+    }
+
+    public double getColumnCount() {
+        return columnCount;
+    }
+
+    public void setColumnCount(double columnCount) {
+        this.columnCount = columnCount;
+    }
+
+    public double getCurrentRow() {
+        return currentRow;
+    }
+
+    public void setCurrentRow(double currentRow) {
+        this.currentRow = currentRow;
+    }
+
+    public double getCurrentColumn() {
+        return currentColumn;
+    }
+
+    public void setCurrentColumn(double currentColumn) {
+        this.currentColumn = currentColumn;
+    }
+
+    public double getCurrentScene() {
+        return currentScene;
+    }
+
+    public void setCurrentScene(double currentScene) {
+        this.currentScene = currentScene;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + (int) (Double.doubleToLongBits(this.rowCount) ^ (Double.doubleToLongBits(this.rowCount) >>> 32));
+        hash = 29 * hash + (int) (Double.doubleToLongBits(this.columnCount) ^ (Double.doubleToLongBits(this.columnCount) >>> 32));
+        hash = 29 * hash + (int) (Double.doubleToLongBits(this.currentRow) ^ (Double.doubleToLongBits(this.currentRow) >>> 32));
+        hash = 29 * hash + (int) (Double.doubleToLongBits(this.currentColumn) ^ (Double.doubleToLongBits(this.currentColumn) >>> 32));
+        hash = 29 * hash + (int) (Double.doubleToLongBits(this.currentScene) ^ (Double.doubleToLongBits(this.currentScene) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Map{" + "rowCount=" + rowCount + ", columnCount=" + columnCount + ", currentRow=" + currentRow + ", currentColumn=" + currentColumn + ", currentScene=" + currentScene + '}';
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Map other = (Map) obj;
+        if (Double.doubleToLongBits(this.rowCount) != Double.doubleToLongBits(other.rowCount)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.columnCount) != Double.doubleToLongBits(other.columnCount)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.currentRow) != Double.doubleToLongBits(other.currentRow)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.currentColumn) != Double.doubleToLongBits(other.currentColumn)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.currentScene) != Double.doubleToLongBits(other.currentScene)) {
+            return false;
+        }
+        return true;
+    }
+    
+    public Map() {
+    }
+    
     
     
 }
