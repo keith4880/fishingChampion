46008ea7e80662cb2e961df07005795c84699f47
Braxton Ward <braxton.ward@vivintsolar.com>
2017-05-13 22:43:45 -0600

    Created actor, location, and game classes. All variables and functions added. Hash and origin still needed.

---
+++ fishingChampion/src/byui/cit260/fishingChampion/model/Actor.java
@@ -0,0 +1,22 @@
+package byui.cit260.fishingChampion.model;
+
+/**
+ * Created by brax10 on 5/13/17.
+ */
+public class Actor {
+    public Actor() {}
+
+    private String name, description, coordinates;
+
+    public String getName() { return name; }
+
+    public void setName( String name ) { this.name = name; }
+
+    public String getDescription() { return  description; }
+
+    public void setDescription( String description ) { this.description = description; }
+
+    public String getCoordinates() { return coordinates; }
+
+    public void setCoordinates( String coordinates) { this.coordinates = coordinates; }
+}
---
+++ fishingChampion/src/byui/cit260/fishingChampion/model/Game.java
@@ -0,0 +1,14 @@
+package byui.cit260.fishingChampion.model;
+
+/**
+ * Created by brax10 on 5/13/17.
+ */
+public class Game {
+    public Game() {}
+
+    private String totalTime;
+
+    public String getTotaltime() { return totalTime; }
+
+    public void setTotalTime(String totalTime) { this.totalTime = totalTime; }
+}
